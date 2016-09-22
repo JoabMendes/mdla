74849d7c5946ed7548d4cbf34464817adc3a3749
JoabMendes <joab.mendes.r2d2@gmail.com>
2016-09-22 00:01:46 -0300

    Made changes to upgrade traminer

--- kdd_1/4_data_mining/TraMineR/miner.R
+++ kdd_1/4_data_mining/TraMineR/miner.R
@@ -2,18 +2,21 @@ library(TraMineR)
 
 #Read from csv
 behaviours <- read.csv(file="~/workspace/mdla_sedis/kdd_1/4_data_mining/TraMineR/input.csv", header=FALSE)
-
+print(" =========== Read sequences ========= ")
 #Transform into state sequences
 #http://traminer.unige.ch/doc/seqdef.html
 behaviours.seq <- seqdef(behaviours, informat="STS")
+print(" =========== Defined sequences ========= ")
 
 #Transforma into event sequences (without transictions)
 #http://traminer.unige.ch/doc/seqecreate.html
 behaviours.seqe <- seqecreate(behaviours.seq, teven="state")
+print(" =========== Turned into event sequences ========= ")
 
 #Fiding the frequent subsequences (Frequency = 51%)
 #http://traminer.unige.ch/doc/seqefsub.html
 fbsubseq <- seqefsub(behaviours.seqe, pMinSupport=0.51)
+print(" =========== Found the frequente subsequences ========= ")
 
 #export to data frame http://stackoverflow.com/questions/25654008/traminer-subseqelist-export-to-data-frame
 #plot(fbsubseq[1:20], col = "cyan")
@@ -29,4 +32,5 @@ decom <- seqdecomp(char_columns)
 #seqiplot(decom.seq , withlegend = True, title="Frequent event sequences 50% Frquence")
 
 #Writing results on csv
+print(" =========== saving ========= ")
 write.table(decom, file="~/workspace/mdla_sedis/kdd_1/4_data_mining/TraMineR/frequent_subsequences.csv",row.names=FALSE, na="",col.names=FALSE, sep=",")
--- kdd_1/4_data_mining/TraMineR/test.R
+++ kdd_1/4_data_mining/TraMineR/test.R
@@ -6,16 +6,21 @@ behaviours <- read.csv(file="~/workspace/mdla_sedis/kdd_1/4_data_mining/TraMineR
 #Transform into state sequences
 #http://traminer.unige.ch/doc/seqdef.html
 behaviours.seq <- seqdef(behaviours, informat="STS")
+print(" =========== Defined sequences ========= ")
+
 
 #Transforma into event sequences (without transictions)
 #http://traminer.unige.ch/doc/seqecreate.html
 behaviours.seqe <- seqecreate(behaviours.seq, teven="state")
+print(" =========== Turned into event sequences ========= ")
 
 #Fiding the frequent subsequences (Frequency = 51%)
 #http://traminer.unige.ch/doc/seqefsub.html
 fbsubseq <- seqefsub(behaviours.seqe, pMinSupport=0.51)
+print(" =========== Found the frequente subsequences ========= ")
 
 #export to data frame http://stackoverflow.com/questions/25654008/traminer-subseqelist-export-to-data-frame
+print(" =========== ploting ========= ")
 plot(fbsubseq, col="cyan")
 #plot(fsubseq[1:15], col = "cyan")
 
@@ -28,5 +33,5 @@ decom <- seqdecomp(char_columns)
 #decom.seq = seqdef(decom, informat="STS")
 #seqiplot(decom.seq , withlegend = True, title="Frequent event sequences 50% Frquence")
 #write.csv(decom, file="/home/mercosul/joab_workspace/mdla_sedis/kdd_1/4_data_mining/TraMineR/test_frequent_subsequences.csv", row.name=FALSE, na="")
-
+print(" =========== saving ========= ")
 write.table(decom, file="~/workspace/mdla_sedis/kdd_1/4_data_mining/TraMineR/test_frequent_subsequences.csv",row.names=FALSE, na="",col.names=FALSE, sep=",")
